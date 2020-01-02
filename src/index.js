dc8195d496349faf0265c1e4258d9da19918fc85
srikantekion <srikantk@tekion.com>
2020-01-03 01:11:31 +0530

    Setup Redux State Management with Devtools

--- src/index.js
+++ src/index.js
@@ -16,10 +16,19 @@ import Login from "./components/Auth/Login";
 
 import "semantic-ui-css/semantic.min.css";
 
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import { setUser } from './actions';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
+          this.props.setUser(user)
         this.props.history.push("/");
       }
     });
@@ -35,12 +44,14 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
 ReactDOM.render(
-  <Router>
-    <RootWithAuth />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 registerServiceWorker();
